bb8e9f352f19dd8ab5657a04a959914d14f4f85f
Liujiajia1994 <460686664@qq.com>
2018-10-25 16:15:57 +0800

    image crop、rotation transform、scale transform

--- data_preprocess.py
+++ data_preprocess.py
@@ -5,24 +5,41 @@ from scipy.misc import imresize
 from PIL import Image
 import os
 import matplotlib.pyplot as plt
+from scipy.ndimage.interpolation import rotate
+import random
+
+data_file = 'F:\\download_SAR_data\\experiment_data\\eddy\\'
+save_file = 'F:\\download_SAR_data\\experiment_data\\dataset\\eddy\\'
 
 
-def defined_crop():
-    for i in range(131):
-        print("正在读取" + str(i) + "个图片")
-        img = cv2.imread('./eddyData/train/ice/' + str(i) + '.tif')
-        img = cv2.resize(img, (280, 280), interpolation=cv2.INTER_CUBIC)
-        img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        crop_img1 = img[1:140, 1:140]
-        crop_img2 = img[140:280, 1:140]
-        crop_img3 = img[1:140, 140:280]
-        crop_img4 = img[140:280, 140:280]
-        crop_img5 = img[70:210, 70:210]
-        cv2.imwrite('./eddyData/train/iceCrop/' + str(i) + '-1.tif', crop_img1)
-        cv2.imwrite('./eddyData/train/iceCrop/' + str(i) + '-2.tif', crop_img2)
-        cv2.imwrite('./eddyData/train/iceCrop/' + str(i) + '-3.tif', crop_img3)
-        cv2.imwrite('./eddyData/train/iceCrop/' + str(i) + '-4.tif', crop_img4)
-        cv2.imwrite('./eddyData/train/iceCrop/' + str(i) + '-5.tif', crop_img5)
+# 裁剪图片的四个280*280，以及中间的一块
+def defined_crop(i, img):
+    h, w = img.shape
+    print('裁剪第'+str(i)+'张图片')
+    crop_img1 = img[1:int(3*h/4), 1:int(3*w/4)]
+    crop_img2 = img[int(h/4):h, 1:int(3*w/4)]
+    crop_img3 = img[1:int(3*h/4), int(w/4):w]
+    crop_img4 = img[int(h/4):h, int(w/4):w]
+    # crop_img5 = img[int(h/4):int(3*h/4), int(w/4):int(3*w/4)]
+    # plt.subplot(161)
+    # plt.imshow(img, cmap='gray')
+    # plt.subplot(162)
+    # plt.imshow(crop_img1, cmap='gray')
+    # plt.subplot(163)
+    # plt.imshow(crop_img2, cmap='gray')
+    # plt.subplot(164)
+    # plt.imshow(crop_img3, cmap='gray')
+    # plt.subplot(165)
+    # plt.imshow(crop_img4, cmap='gray')
+    # plt.subplot(166)
+    # plt.imshow(crop_img5, cmap='gray')
+    # plt.show()
+    print('保存第'+str(i)+'张图片')
+    cv2.imwrite(save_file + str(i) + 'crop-1.tif', crop_img1)
+    cv2.imwrite(save_file + str(i) + 'crop-2.tif', crop_img2)
+    cv2.imwrite(save_file + str(i) + 'crop-3.tif', crop_img3)
+    cv2.imwrite(save_file + str(i) + 'crop-4.tif', crop_img4)
+    # cv2.imwrite(save_file + str(i) + 'crop-5.tif', crop_img5)
 
 
 def check_size(size):
@@ -33,11 +50,26 @@ def check_size(size):
     return size
 
 
+# 随机裁剪
 def random_crop(image, crop_size):
     crop_size = check_size(crop_size)
     h, w = image.shape
     top = np.random.randint(0, h - crop_size[0])
     left = np.random.randint(0, w - crop_size[1])
+    # # 随机生成不重复的整数
+    # top = random.sample(range(1, 9), 1)[0]*10
+    # left = random.sample(range(1, 9), 1)[0]*10
+    bottom = top + crop_size[0]
+    right = left + crop_size[1]
+    image = image[top:bottom, left:right]
+    return image
+
+
+def center_crop(image, crop_size):
+    crop_size = check_size(crop_size)
+    h, w = image.shape
+    top = (h - crop_size[0]) // 2
+    left = (w - crop_size[1]) // 2
     bottom = top + crop_size[0]
     right = left + crop_size[1]
     image = image[top:bottom, left:right]
@@ -50,9 +82,10 @@ def save_image(image, imagefile):
     image.save(imagefile)
 
 
+# 文件重命名
 def file_rename():
     count = 0
-    path = "F:\\download_SAR_data\\experiment_data\\sea_water\\"
+    path = save_file
     filelist = os.listdir(path)#该文件夹下所有的文件（包括文件夹）
     for files in filelist:#遍历所有文件
         Olddir = os.path.join(path,files)#原来的文件路径
@@ -60,30 +93,67 @@ def file_rename():
             continue
         filename = os.path.splitext(files)[0]#文件名
         filetype = os.path.splitext(files)[1]#文件扩展名
-        Newdir = os.path.join(path,str(count)+filetype)#新的文件路径
+        Newdir = os.path.join(path, 'eddy-'+str(count)+filetype)#新的文件路径
         os.rename(Olddir,Newdir)#重命名
         count += 1
 
 
+# 尺度变换
+def scale_augmentation(image, i, crop_size):
+    # scale_size = np.random.randint(*scale_range)
+    scale_size = (i+2)*100
+    image = imresize(image, (scale_size, scale_size))
+    # image = random_crop(image, crop_size)
+    image = center_crop(image, crop_size)
+    return image
+
+
+def resize(image, size):
+    size = check_size(size)
+    image = imresize(image, size)
+    return image
+
+
+# 旋转
+def random_rotation(i,image):
+    h, w = image.shape
+    angle = i*30
+    image = rotate(image, angle)
+    image = resize(image, (h, w))
+    return image
+
+
+# 生成5张图片
+def generate_image(i, image, arg):
+    count = 1
+    while 1:
+        if( arg == 'rotation'):
+            images = random_rotation(count, image)
+        else:
+            images = scale_augmentation(image, count, 280)
+        # # 显示图片
+        # plt.subplot(121)
+        # plt.imshow(image, cmap='gray')
+        # plt.subplot(122)
+        # plt.imshow(images, cmap='gray')
+        # plt.show()
+        print('正在保存第' + str(i) + '-' + str(count) + '张图片')
+        cv2.imwrite(save_file + str(i) + arg + '-' + str(count) + '.tif', images)
+        count += 1
+        if count == 6:
+            break
+
+
 if __name__ == '__main__':
     file_rename()
-    # data_file = 'F:\\download_SAR_data\\experiment_data\\no_processed_big_land_data\\'
-    # for i in range(8):
-    #     print('正在读取第'+str(i+1)+'张图片')
-    #     image = cv2.imread(data_file+str(i+1)+'.tif')
-    #     image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    #     # image_size = resize(image,280)
-    #     print('正在随机剪切第' + str(i + 1) + '张图片')
-    #     count = 1
-    #     while 1:
-    #         image_crop = random_crop(image, 280)
-    #         # 显示图片
-    #         # plt.subplot(151)
-    #         # plt.imshow(image_crop, cmap='gray')
-    #         # plt.show()
-    #         print('正在保存第' + str(i+1)+'-'+str(count) + '张图片')
-    #         # save_image(image_crop, 'F:\\download_SAR_data\\experiment_data\\land\\test_data\\'+str(i+1)+'.tif')
-    #         cv2.imwrite('F:\\download_SAR_data\\experiment_data\\land\\'+str(i+1)+'-'+str(count)+'.tif', image_crop)
-    #         count += 1
-    #         if count == 251:
-    #             break
+    # for i in range(136):
+    #     print('正在读取第'+str(i)+'张图片')
+    #     image = cv2.imread(data_file + str(i) + '.tif', 0)
+    #     if image is not None:
+    #         defined_crop(i, image)
+    #         generate_image(i, image, 'rotation')
+    #         generate_image(i, image, 'scale')
+    #     else:
+    #         print('无法读取第'+str(i)+'张图片')
+
+
